2382d35e8326351da358d8d77cec2ea0d8ba69ce
jiwon1118 <b23386585@gmail.com>
2025-03-17 12:47:56 +0900

    0.1.0

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import pandas as pd 
+import pyarrow
 
 BASE_URL="http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
 KEY=os.getenv("MOVIE_KEY")
@@ -21,12 +22,24 @@ def call_api(dt="20120101", url_param={}):
     return data['boxOfficeResult']["dailyBoxOfficeList"]
 
 
-
-
 def list2df(data : list , dt : str):
     l = call_api()
     # TODO = list를 DataFrame으로 변환
     df = pd.DataFrame(l)
-    #df['dt'] = dt 도 가능 (맨 뒤에 생김)
-    df.insert(0, "dt", dt)
+    df['dt'] = dt #도 가능 (맨 뒤에 생김)
+    #df.insert(0, "dt", dt)
+    num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt',
+                'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten',
+                'salesChange', 'audiInten', 'audiChange']
+    for col_name in num_cols:
+        df[col_name] = pd.to_numeric(df[col_name])
+    # df[num_cols] = df[num_cols].apply(pd.to_numeric) - 이것도 가능
+    
     return df
+
+def save_df(df : pd.DataFrame , base_path) -> str:
+    # list2df를 base_path에 파일로 저장
+    df.to_parquet(base_path, partition_cols=['dt'])
+    path = f"{base_path}/dt={df['dt'][0]}"
+    return path
+
--- tests/test_call.py
+++ tests/test_call.py
@@ -1,6 +1,8 @@
-from movie.api.call import gen_url, call_api, list2df
+from movie.api.call import gen_url, call_api, list2df, save_df
 import os
 import pandas as pd 
+import pyarrow
+from pandas.api.types import is_numeric_dtype
 
 def test_gen_url():
     r = gen_url()
@@ -36,3 +38,34 @@ def test_list2df():
     assert set(data[0].keys()).issubset(set(df.columns))
     assert "dt" in df.columns, "df 컬럼이 있어야 함"
     assert (df["dt"] == ymd).all(), "모든 컬럼에 입력된 날짜 값이 존재 해야 함"
+    
+def test_save_df():
+    ymd = "20210101"
+    data = call_api(dt=ymd)
+    df = list2df(data, ymd)
+    base_path = "~/temp/movie"
+    r = save_df(df, base_path)
+    assert r == f"{base_path}/dt={ymd}"
+    read_df = pd.read_parquet(r)
+    assert 'dt' not in read_df.columns
+    assert 'dt' in pd.read_parquet(base_path).columns
+    
+    
+def test_list2df_check_num():
+    """df 에 숫자 컬럼을 변환 하고 잘 변환 되었는가 확인인"""
+    num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt',
+                'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten',
+                'salesChange', 'audiInten', 'audiChange']
+    
+    # hint - 변환 : df[num_cols].apply(pd.to_numeric)
+    # hint - 확인 : is_numeric_dtypes <- pandas ...
+    ymd = "20210101"
+    data = call_api(dt=ymd)
+    df = list2df(data, ymd)
+    
+    for c in num_cols:
+        assert df[c].dtype in ['int64' ,'float64'], f"{c} 가 숫자가 아님"
+        assert is_numeric_dtype(df[c])
+ 
+    
+    
