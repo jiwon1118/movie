d058b2e8aeef2aa9d66695d6bbdad6b74af0e617
jiwon1118 <b23386585@gmail.com>
2025-03-18 11:26:11 +0900

    0.2.1

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -10,9 +10,9 @@ def gen_url(dt="20120101", url_param={}):
     "호출 URL 생성, url_param이 입력되면 multiMovieYn, repNationCd 처리"
     url = f"{BASE_URL}?key={KEY}&targetDt={dt}"
     # TODO = url_param 처리
-    if url_param:
-        for k, v in url_param.items():
-            url += f"&{k}={v}"
+    for k, v in url_param.items():
+        url += f"&{k}={v}"
+        
     return url
 
 
