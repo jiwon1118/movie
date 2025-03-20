1d3fe1035793244e7e131de627413337a4f47229
jiwon1118 <b23386585@gmail.com>
2025-03-20 16:25:26 +0900

    0.4.2

--- src/movie/api/after.py
+++ src/movie/api/after.py
@@ -28,10 +28,11 @@ def fillna_meta(previous_df: pd.DataFrame, current_df: pd.DataFrame) -> pd.DataF
 def save_meta(df: pd.DataFrame, parquet_path: str, partitions: list) -> str:
     os.makedirs(os.path.dirname(parquet_path), exist_ok=True)
 
-    df.to_parquet(parquet_path, partition_cols=partitions, index=False)
-    
+    if partitions:
+        df.to_parquet(parquet_path, partition_cols=partitions, index=False)
+    else:
+        df.to_parquet(parquet_path, index=False)
+
     # 저장된 파일의 절대 경로 반환
-    save_path = os.path.abspath(parquet_path)
-    print(f"파일이 성공적으로 저장되었습니다: {save_path}")
-    
+    save_path = os.path.abspath(parquet_path)    
     return save_path
